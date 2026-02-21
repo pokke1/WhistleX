84feecfea3d11d191a5ad31a7d6f50b685be3a04
NATIVE <native@NATIVEs-Mac-mini.local>
2026-02-21 02:52:29 +0100

    Revert "Relax express types for middleware and json options"
    This reverts commit 443d746971a73714a0cdd29121ac0678dc6f21bf.

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -29,19 +29,19 @@ declare module "express" {
   }
 
   export interface Router {
-    get: (path: string, ...handlers: RequestHandler[]) => Router;
-    post: (path: string, ...handlers: RequestHandler[]) => Router;
+    get: (path: string, handler: RequestHandler) => Router;
+    post: (path: string, handler: RequestHandler) => Router;
   }
 
   export interface Express {
     use: (...handlers: RequestHandler[] | any[]) => void;
-    get: (path: string, ...handlers: RequestHandler[]) => Express;
-    post: (path: string, ...handlers: RequestHandler[]) => Express;
+    get: (path: string, handler: RequestHandler) => Express;
+    post: (path: string, handler: RequestHandler) => Express;
     listen: (port: number, callback?: () => void) => any;
   }
 
   export interface JsonParser {
-    (options?: any): RequestHandler;
+    (): RequestHandler;
   }
 
   export interface ExpressModule {
